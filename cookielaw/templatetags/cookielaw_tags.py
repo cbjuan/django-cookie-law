54dc2d75e7dc7d40adc60457269bce86215bd9ca
juancb <juancb@usal.es>
2017-04-07 18:44:56 +0200

    Rollback. The error in Django 1.11 is not fixed

--- cookielaw/templatetags/cookielaw_tags.py
+++ cookielaw/templatetags/cookielaw_tags.py
@@ -3,24 +3,12 @@
 from django import template
 from django.template.loader import render_to_string
 
-import django
 
 register = template.Library()
 
+
 @register.simple_tag(takes_context=True)
 def cookielaw_banner(context):
-    
     if context['request'].COOKIES.get('cookielaw_accepted', False):
         return ''
-    
-    request = context.get('request', None)
-    
-    data = self.get_context(context, **kwargs)
-    
-    
-    if django.VERSION[:2] < (1, 10):
-            return render_to_string(
-                'cookielaw/banner.html', data, context_instance=context
-            )
-    else:
-        return render_to_string('cookielaw/banner.html', data, request)
+    return render_to_string('cookielaw/banner.html', context)
