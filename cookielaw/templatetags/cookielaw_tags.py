cdee49a0ed600a72955d844abf5e4b5b2f9970cc
juancb <juancb@usal.es>
2017-04-07 18:32:12 +0200

    Fix required for Django 1.11
    Update to fix "context must be a dict rather than RequestContext" error in Django 1.11

--- cookielaw/templatetags/cookielaw_tags.py
+++ cookielaw/templatetags/cookielaw_tags.py
@@ -9,7 +9,18 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def cookielaw_banner(context):
+    
     if context['request'].COOKIES.get('cookielaw_accepted', False):
         return ''
-    return render_to_string('cookielaw/banner.html', context)
-
+    
+    request = context.get('request', None)
+    
+    data = self.get_context(context, **kwargs)
+    
+    
+    if django.VERSION[:2] < (1, 10):
+            return render_to_string(
+                'cookielaw/banner.html', data, context_instance=context
+            )
+        else:
+            return render_to_string('cookielaw/banner.html', data, request)
